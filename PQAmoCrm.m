265f2ad43f31884dc2dc6ece1bc9ad68d8cefbe1
Maximryzhkov <Maximryzhkov@users.noreply.github.com>
2019-03-06 16:51:56 +0300

    Update PQAmoCrm.m

--- PQAmoCrm.m
+++ PQAmoCrm.m
@@ -2,7 +2,7 @@ let
     sourceFn = Expression.Evaluate(
         Text.FromBinary(
             Binary.Buffer(
-                Web.Contents("https://raw.githubusercontent.com/ezabitov/PQAmoCrm/master/main.m")
+                Web.Contents("https://raw.githubusercontent.com/Maximryzhkov/PQAmoCrm/master/main.m")
             )
         ), #shared)
 in
